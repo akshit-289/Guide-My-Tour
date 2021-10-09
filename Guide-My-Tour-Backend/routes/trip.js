6e4fffc1fbd06de680ead01dcce0a00b3bb966c5
akshit-289 <aggarwal.akshit289@gmail.com>
2021-10-10 00:03:31 +0530

    Add files via upload

--- Guide-My-Tour-Backend/routes/trip.js
+++ Guide-My-Tour-Backend/routes/trip.js
@@ -1,23 +1,16 @@
 const express = require('express'); //import express
-const multer = require('multer');
-const upload = multer();
-
 
 // 1.
 const router  = express.Router(); 
 // 2.
 const tripController = require('../controllers/trip'); 
 // 3.
-router.post('/trip', upload.none(), tripController.newTrip); 
+router.post('/trip', tripController.uploadImg, tripController.newTrip); 
 // 4. 
 router.get('/trip', tripController.getAllTrip);
 //5.
 router.delete('/trip', tripController.deleteAllTrip);
 //6.
 router.get('/trip/:name', tripController.getOneTrip);
-//7.
-router.post('/trip/:name', tripController.newComment);
-//8.
-router.delete('/trip/:name', tripController.deleteOneTrip);
 
 module.exports = router; // export to use in app.js
